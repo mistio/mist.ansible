49d0d68a441a2ded5578226d8a02aa62ea9e817e
commixon <commixon@gmail.com>
2014-09-09 15:43:00 +0300

    Fix in version 0.1.2

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ requires = [
 
 setup(
     name='mist.ansible',
-    version='0.1.1',
+    version='0.1.2',
     description='Ansible modules for the mist.io service',
     long_description=readme(),
     classifiers=[
